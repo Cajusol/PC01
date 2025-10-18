de550526573ebf626b0e655a39344e9c6c0fa060
Marco <mcajusol@gmail.com>
2025-10-18 08:30:56 -0500

    Primer

--- app/src/main/java/com/mcajusol/pc01/MainActivity.kt
+++ app/src/main/java/com/mcajusol/pc01/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mcajusol.pc01.ui.theme.PC01Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +32,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PC01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            SimpleApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+@Preview
+fun SimpleApp() {
+    var name by remember { mutableStateOf(value = "") }
+    var message by remember { mutableStateOf<String?>(value = null) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PC01Theme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Spacer(modifier = Modifier.width(width = 12.dp))
+        //Text(text = "Hola Mundo", style = MaterialTheme.typography.titleMedium)
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text(text = "Escribe tu nombre") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+        Button(
+            onClick = {
+                val safeName = if(name.isBlank()) "Desconocido" else name
+                message = "Hola $safeName"
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Saludar")
+        }
+        message?.let {
+            Text(text = it,style=MaterialTheme.typography.titleMedium)
+        }
     }
+
 }
